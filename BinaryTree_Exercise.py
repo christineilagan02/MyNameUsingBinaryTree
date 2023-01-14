23d02cee6182fff1c9501c1f6c508eb094a0acef
unknown <christineilagan02@gmail.com>
2023-01-14 11:38:26 +0800

    Implementing in order traversal method

---
+++ BinaryTree_Exercise.py
@@ -0,0 +1,44 @@
+# Binary Tree 
+# src: https://youtu.be/lFq5mYUWEBk
+
+class BinarySearchTreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+        
+    # Checking the value 
+    def add_child(self, data):
+        if data == self.data:
+            return
+        
+        if data < self.data:
+            # add data in left subtree
+            if self.left:
+                self.left.add_child(data)
+            else:
+                self.left = BinarySearchTreeNode(data)
+        else:
+            # add data in right subtree
+            if self.right:
+                self.right.add_child(data)
+            else:
+                self.right = BinarySearchTreeNode(data)
+                
+    # Implementing in order traversal method
+    def in_order_traversal(self):
+        elements = []
+        
+        # visit the left tree
+        if self.left:
+            elements += self.left.in_order_traversal()
+            
+        # visit the base node
+        elements.append(self.data)
+        
+        # visit the right tree
+        if self.right:
+            elements += self.right.in_order_traversal()
+        
+        return elements
+    
