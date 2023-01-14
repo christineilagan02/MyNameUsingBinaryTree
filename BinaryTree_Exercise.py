4bc7e68992af2ac8c575627726ecd8dd8ea4d2af
unknown <christineilagan02@gmail.com>
2023-01-14 11:53:03 +0800

    I added build tree and also the list of numbers

--- BinaryTree_Exercise.py
+++ BinaryTree_Exercise.py
@@ -42,3 +42,18 @@ class BinarySearchTreeNode:
         
         return elements
     
+# build tree
+def build_tree(elements):
+    root = BinarySearchTreeNode(elements[0])
+    
+    for i in range(1, len(elements)):
+        root.add_child(elements[i])
+        
+    return root
+  
+# list off numbers 
+if __name__ == '__main__':
+    numbers = [17, 4 , 1, 20, 9, 23, 18, 34]
+    numbers_tree = build_tree(numbers)
+    print(numbers_tree.in_order_traversal())
+    
